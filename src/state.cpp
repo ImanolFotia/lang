7d0e9c169ef6388d1bb6e6842a90fe953c6d6d7f
Imanol Fotia <imanolfotia@gmail.com>
2025-05-20 03:15:45 -0300

    Included scope variables in state

--- src/state.cpp
+++ src/state.cpp
@@ -2,5 +2,4 @@
 
 std::unordered_map<std::string, std::shared_ptr<Node>> State::vars;
 std::unordered_map<std::string, std::shared_ptr<Node>> State::functions;
-Test State::test;
-std::string State::name = "setted name";
+std::unordered_map<std::string, std::shared_ptr<Node>> State::scope_variables;
